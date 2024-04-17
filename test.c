e55eac948e9981da02a2a43da5946cdbddb4a68e
zys1310992814 <1310992814@qq.com>
2024-04-18 02:24:48 +0800

    first commit

--- test.c
+++ test.c
@@ -2,18 +2,24 @@
  
 int main(void)
 {
-    int *sa=0x0804c010;  //桩的地址
-    int *sb=0x0804c01c;  //桩的地址
+    int *sa=0x0804c00c;  //桩的地址
+    int *sb=0x0804c010;  //桩的地址
     int *sc=0x0804c014;  //桩的地址
-	printf("The address of first call function test_a is %p\n", *sa); //第1次调用时的地址
-	printf("The address of first call function test_b is %p\n", *sb); //第1次调用时的地址
-    printf("The address of first call function test_c is %p\n", *sc); //第1次调用时的地址
+    int *sd=0x0804c018;  //桩的地址
+    int *se=0x0804c01c;  //桩的地址
+	printf("The address of first call function printf@GLIBC_2.0 is %p\n",             *sa); //第1次调用时的地址
+	printf("The address of first call function test_a is %p\n",                       *sb); //第1次调用时的地址
+    printf("The address of first call function test_c is %p\n",                       *sc); //第1次调用时的地址
+    printf("The address of first call function __libc_start_main@GLIBC_2.0 is %p\n",  *sb); //第1次调用时的地址
+    printf("The address of first call function test_b is %p\n",                       *se); //第1次调用时的地址
     test_a();
     test_b();
     test_c();
-	printf("The address of second call function test_a is %p\n", *sa); //第2次调用时的地址
-	printf("The address of second call function test_b is %p\n", *sb); //第2次调用时的地址
-    printf("The address of second call function test_c is %p\n", *sc); //第2次调用时的地址
+	printf("The address of first call function printf@GLIBC_2.0 is %p\n",             *sa); //第1次调用时的地址
+	printf("The address of first call function test_a is %p\n",                       *sb); //第1次调用时的地址
+    printf("The address of first call function test_c is %p\n",                       *sc); //第1次调用时的地址
+    printf("The address of first call function __libc_start_main@GLIBC_2.0 is %p\n",  *sb); //第1次调用时的地址
+    printf("The address of first call function test_b is %p\n",                       *se); //第1次调用时的地址
     while(1);
     return 0;
 }
